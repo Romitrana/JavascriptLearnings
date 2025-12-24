3bf8b9e7162df858f5b8763d1166c3cd2687a094
Romit Rana <romit2rana@gmail.com>
2025-12-24 23:43:24 +0530

    learn null, undefined, boolean

--- JS/datatypes.js
+++ JS/datatypes.js
@@ -13,5 +13,44 @@ String	    "string"	                    String
 Symbol	    "symbol"	                    Symbol
 */
 
+// ----------------------null-----------------------
+// console.log(typeof null) //object its's bug
+// console.log("romit"+null) //romitnull -> null converted to string and appended to "romit"
+// console.log(null==undefined) //true
+// console.log(null===undefined) //false
+//"use strict" Strict mode makes JavaScript safer, cleaner, and less error-prone by enforcing stricter rules.
+//let a = 5;
+//b = a; //under "use strict" Mode it will throw a reference error
+//console.log(b);
 
-// ----------------------Null-----------------------
+//---------------------undefined------------------------
+// console.log(a) //cannot be accessed before initialization in case of let &n const in case of var it will be undefined
+// const a=4;
+
+// let a;
+// console.log(a); //undefined
+
+// const obj = {
+//   name: "romit",
+//   age: 24,
+// };
+// console.log(obj.name); //romit
+// console.log(obj.address); //undefined
+
+//--------------------boolean---------------------
+//truthy values
+// console.log(Boolean(true))
+// console.log(Boolean(1))
+// console.log(Boolean("romit"))
+// console.log(Boolean("false"))
+// console.log(Boolean([]))
+// console.log(Boolean({}))
+// console.log(Boolean(function(){}))
+// console.log(Boolean("0"))
+
+//falsy values
+// console.log(Boolean(false))
+// console.log(Boolean(0))
+// console.log(Boolean(''))
+// console.log(Boolean(""))
+// console.log(Boolean(NaN))
